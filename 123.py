23d50dde8fe600fced72f5a8b277d61dcc63e631
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:06:19 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -6,20 +6,21 @@ if confirm_input:
   a=random.randint(2,99)
   start,end=1,100
   b=st.number_input("請輸入%d到%d之間的整數:"%(start,end))
-  if b:
-    if b==a:
-      st.write("恭喜你中獎了")
+  if confirm_input1 :
+    if b:
+      if b==a:
+       st.write("恭喜你中獎了")
     
-  elif b>a:
-    if b>=end:
-      st.write("輸入不合法,請重新輸入:")
+    elif b>a:
+      if b>=end:
+        st.write("輸入不合法,請重新輸入:")
+      else:
+       end=b
     else:
-      end=b
-  else:
-     if b<=start:
-       st.write("輸入不合法,請重新輸入:")
-     else:
-       st.write=b
+      if b<=start:
+        st.write("輸入不合法,請重新輸入:")
+      else:
+        st.write=b
 #if __name__=='__main__':
   #paly()
 
