4411c085a55cdde3a05b28441b4c856c51a3a9ff
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:19:30 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -6,7 +6,7 @@ def paly():
   a=random.randint(2,99)
   start,end=1,100
   while 1:
-    b=number_input("請輸入%d到%d之間的整數:"%(start,end))
+    b=st.number_input("請輸入%d到%d之間的整數:"%(start,end))
     if b==a:
       st.write("恭喜你中獎了")
       break
@@ -20,5 +20,5 @@ def paly():
         st.write("輸入不合法,請重新輸入:")
       else:
         start=b
-if __name__=='__main__':
-  paly()
+#if __name__=='__main__':
+  #paly()
