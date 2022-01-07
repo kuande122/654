6cfb190077dfc42c1fc3a6c82bace10f001c6c90
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:14:38 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -13,17 +13,17 @@ while True:
     try:
         guess = int(guess)  #把字串轉換成整數
     except ValueError:  #轉換失敗便要求重新輸入數字
-        print('格式錯誤，請輸入數字\n')
+        st.write('格式錯誤，請輸入數字\n')
         continue
     
     #檢查輸入的數字是否介於規定範圍內
     if guess <= lowest or guess >= highest:
-        print('請輸入 ' + str(lowest) + '-' + str(highest) + ' 之間的整數\n')
+        st.write('請輸入 ' + str(lowest) + '-' + str(highest) + ' 之間的整數\n')
         continue
     
     #判斷有沒有猜中密碼
     if guess == answer:
-        print('答對了！')
+        st.write('答對了！')
         break   #猜對才跳脫迴圈
     elif guess < answer:
         lowest = guess
