06a923676a9d441602e9645bca609c3b59eb7fa5
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:28:52 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -5,7 +5,7 @@ if confirm_input:
   c=random.randint(2,99)
   start=1
   end=100
-x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
+x=st.number_input("請輸入%g到%g之間的整數:"(start,end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
   if x==c:
