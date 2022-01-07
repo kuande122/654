a788427aa330e79167a754b22f8692557cab9892
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:21:28 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,24 +1,31 @@
-from random import randint
-import streamlit as st
-__author__="teacher"
 import random
-def paly():
-  a=random.randint(2,99)
-  start,end=1,100
-  while 1:
-    b=st.number_input("請輸入%d到%d之間的整數:"%(start,end))
-    if b==a:
-      st.write("恭喜你中獎了")
-      break
-    elif b>a:
-      if b>=end:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-        end=b
-    else:
-      if b<=start:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-        start=b
-#if __name__=='__main__':
-  #paly()
+import streamlit as st
+confirm_input = st.button('確認產生答案')
+if confirm_input:
+st.session_state.c = c = random.randint(2,99)
+st.session_state.start = 1
+st.session_state.end = 100
+st.session_state.start,st.session_state.end = 1,100
+#st.write('c=', st.session_state.c)
+
+
+
+
+
+x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
+#st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!")
+confirm_input2 = st.button('輸入確認')
+if confirm_input2:
+if x==st.session_state.c:
+  st.write("恭喜你中獎了")
+elif x>st.session_state.c:
+ if x>=st.session_state.end:
+  st.write("輸入不合法,請重新輸入:")
+ else:
+  st.session_state.end=x
+else:
+ if x<=st.session_state.start:
+  st.write("輸入不合法,請重新輸入:")
+ else:
+  st.session_state.start=x
+
