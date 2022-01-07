3078b7a0aef701bce7b5fc3b6c7c943b3371e529
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 17:05:54 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -4,17 +4,15 @@ import streamlit as st
 #規定範圍並產生密碼
 lowest = 1
 highest = 100
-answer = random.randint(lowest, highest)
-guess=st.number_input("請輸入%g到%g之間的整數:"%(lowest,highest))
+a = random.randint(lowest, highest)
+x=st.number_input("請輸入%g到%g之間的整數:"%(lowest,highest))
 st.write(answer)
 #重複猜數字，直到猜對為止
 while True:
  #判斷有沒有猜中密碼
-    if guess==answer:
-       st.write('答對了！')
-        break   #猜對才跳脫迴圈
-    elif guess<answer:
-        lowest=guess
-    else:
-        highest=guess
+   if x==a:
+     st.write("恭喜你中獎了")
+ 
+
+
 
