6d8c4cc7cdcdd24ed9d523923dc6cf1254ed4ce9
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:57:40 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -7,14 +7,7 @@ answer = randint(lowest, highest)
 guess=st.number_input("請輸入%g到%g之間的整數:"%(lowest,highest))
 #重複猜數字，直到猜對為止
 while True:
-    guess = st.write('密碼介於 ' + str(lowest) + '-' + str(highest) + ':\n>>')
- 
-        #檢查輸入的數字是否介於規定範圍內
-    #if guess <= lowest or guess >= highest:
-        #st.write('請輸入 ' + str(lowest) + '-' + str(highest) + ' 之間的整數\n')
-        #continue
- 
-        #判斷有沒有猜中密碼
+ #判斷有沒有猜中密碼
     if guess == answer:
         st.write('答對了！')
         break   #猜對才跳脫迴圈
