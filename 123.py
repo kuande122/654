ff0fff1604f0a448e34c59fc9dbe6f088145877e
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:12:22 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,27 +1,31 @@
-import random
-import streamlit as st
-confirm_input = st.button('開始遊戲')
-confirm_input1 = st.button('輸入確認')
-if confirm_input:
-  a=random.randint(2,99)
-  start,end=1,100
-  b=st.number_input("請輸入%d到%d之間的整數:"%(start,end))
-  if confirm_input1 :
-    st.write('人')
-    if b:
-      if b==a:
-       st.write("恭喜你中獎了")
-    
-    elif b>a:
-      if b>=end:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-       end=b
-    else:
-      if b<=start:
-        st.write("輸入不合法,請重新輸入:")
-      else:
-        st.write=b
-#if __name__=='__main__':
-  #paly()
+from random import randint
 
+#規定範圍並產生密碼
+lowest = 1
+highest = 100
+answer = randint(lowest, highest)
+
+#重複猜數字，直到猜對為止
+while True:
+    guess = input('密碼介於 ' + str(lowest) + '-' + str(highest) + ':\n>>')
+    st.write(guess)
+    #檢查輸入的內容是否為數字
+    try:
+        guess = int(guess)  #把字串轉換成整數
+    except ValueError:  #轉換失敗便要求重新輸入數字
+        print('格式錯誤，請輸入數字\n')
+        continue
+    
+    #檢查輸入的數字是否介於規定範圍內
+    if guess <= lowest or guess >= highest:
+        print('請輸入 ' + str(lowest) + '-' + str(highest) + ' 之間的整數\n')
+        continue
+    
+    #判斷有沒有猜中密碼
+    if guess == answer:
+        print('答對了！')
+        break   #猜對才跳脫迴圈
+    elif guess < answer:
+        lowest = guess
+    else:
+        highest = guess
