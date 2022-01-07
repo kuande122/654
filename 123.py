84e3b8bd87920c1243fc480d411d0eea2b1ae850
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:31:58 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,16 +1,16 @@
 import random
 import streamlit as st
 confirm_input = st.button('開始遊戲')
-start=1
-end=100
+
 if confirm_input:
   c=random.randint(1,100)
-  
-x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
-confirm_input2 = st.button('輸入確認')
-if confirm_input2:
-  if x==c:
-    st.write("恭喜你中獎了")
+  start=1
+  end=100
+  x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
+  confirm_input2 = st.button('輸入確認')
+  if confirm_input2:
+    if x==c:
+      st.write("恭喜你中獎了")
   elif x>c:
     if x>=end:
       st.write("輸入不合法,請重新輸入:")
