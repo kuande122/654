c3a5e48801adf0eb8b9f313268421568e133f265
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:23:41 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -16,13 +16,13 @@ x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.
 #st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!")
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
-if x==st.session_state.c:
-  st.write("恭喜你中獎了")
-elif x>st.session_state.c:
- if x>=st.session_state.end:
-  st.write("輸入不合法,請重新輸入:")
- else:
-  st.session_state.end=x
+  if x==st.session_state.c:
+    st.write("恭喜你中獎了")
+  elif x>st.session_state.c:
+    if x>=st.session_state.end:
+      st.write("輸入不合法,請重新輸入:")
+    else:
+      st.session_state.end=x
 else:
  if x<=st.session_state.start:
   st.write("輸入不合法,請重新輸入:")
