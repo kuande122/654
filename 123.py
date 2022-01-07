c226cd0f4521ebfe338d6a333f89ccedcda27910
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 17:32:38 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,13 +1,13 @@
 import random
 import streamlit as st
 confirm_input = st.button('確認產生答案')
-
-c=random.randint(2,99)
-start = 1
-end = 100
-start,end = 1,100
-st.write(c)
-x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
+if confirm_input:
+  c=random.randint(2,99)
+  start = 1
+  end = 100
+  start,end = 1,100
+  st.write(c)
+  x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
 if x==c:
   st.write('恭喜你中獎')
   
