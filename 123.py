e3a11c3b8e1c88bf4ee975db4acd5c5ae22e97aa
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:38:10 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -8,22 +8,22 @@ if confirm_input:
   end=100
   x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
   confirm_input2 = st.button('輸入確認')
-  while confirm_input2:
+  if confirm_input2:
     if x==c:
       st.write("恭喜你中獎了")
-      break
+     
     elif x>c:
       if x>=end:
         st.write("輸入不合法,請重新輸入:")
-        continue
+        
       else:
         end=x
-        continue
+        
     else:
       if x<=start:
         st.write("輸入不合法,請重新輸入:")
-        continue
+       
       else:
         start=x
-        continue
+      
 
