76e4a2230067936b825f0ef4767c57671c81af48
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 16:28:39 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,31 +1,23 @@
 import random
 import streamlit as st
-confirm_input = st.button('確認產生答案')
+confirm_input = st.button('開始遊戲')
 if confirm_input:
-  st.session_state.c=c=random.randint(2,99)
-  st.session_state.start = 1
-  st.session_state.end = 100
-  st.session_state.start,st.session_state.end = 1,100
-  #st.write('c=', st.session_state.c)
-
-
-
-
-
-x=st.number_input("請輸入%g到%g之間的整數:"%(st.session_state.start,st.session_state.end))
-#st.write("「輸入確認」鍵記得按兩次喔,否則可能導致程式無法正常運行!")
+  c=random.randint(2,99)
+  start=1
+  end=100
+x=st.number_input("請輸入%g到%g之間的整數:"%(start,end))
 confirm_input2 = st.button('輸入確認')
 if confirm_input2:
-  if x==st.session_state.c:
+  if x==c:
     st.write("恭喜你中獎了")
-  elif x>st.session_state.c:
-    if x>=st.session_state.end:
+  elif x>c:
+    if x>=end:
       st.write("輸入不合法,請重新輸入:")
     else:
-      st.session_state.end=x
+      end=x
 else:
- if x<=st.session_state.start:
+ if x<=start:
   st.write("輸入不合法,請重新輸入:")
  else:
-  st.session_state.start=x
+  start=x
 
