c8e1d289af4396fd4c7cc3f1d932f81ba0089cae
kuande122 <95857538+kuande122@users.noreply.github.com>
2022-01-07 17:30:24 +0800

    Update 123.py

--- 123.py
+++ 123.py
@@ -2,11 +2,11 @@ import random
 import streamlit as st
 confirm_input = st.button('確認產生答案')
 
-  c=random.randint(2,99)
-  start = 1
-  end = 100
-  start,end = 1,100
-  st.write(c)
+c=random.randint(2,99)
+start = 1
+end = 100
+start,end = 1,100
+st.write(c)
 x=st.number_input("請輸入1到100之間的整數:")
 if x==c:
   st.write('恭喜你中獎')
